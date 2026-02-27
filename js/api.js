4df29250732147172ee3c8479dc96873ae703d3c
idriswo <irgashevidris2@gmail.com>
2026-02-27 14:49:29 +0500

    scond commit

---
+++ js/api.js
@@ -0,0 +1,26 @@
+import { ShowData } from "./dom.js"
+let api = "http://localhost:3001/data"
+
+
+export async function GetData() {
+    try {
+        const response = await axios.get(api)
+        return response.data
+    } catch (error) {
+        console.error(error);
+
+    }
+}
+
+export async function appendData() {
+    let data = await GetData()
+    ShowData(data)
+}
+git add 
+export async function checkData(obj) {
+    try {
+        await axios.put(`${api}/${obj.id}`, obj)
+    } catch (error) {
+        console.error(error);
+    }
+}
